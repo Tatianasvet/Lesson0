92c6a9e002b1f68a583e833b60d9229e9227d32e
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-04-29 22:30:26 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var= (1, 2, 'bag','case', 'box')
+print(immutable_var)
+#immutable_var[1]=5
+#print(immutable_var)
+mutable_list=[5, 7, 'bag','case', 'box']
+print(mutable_list)
+mutable_list[2]=10
+print(mutable_list)
