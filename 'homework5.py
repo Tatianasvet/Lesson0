a09fa934901048e5979b92bb30d1a858fbc4d67d
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-04-30 18:04:09 +0500

    Add files via upload

---
+++ 'homework5.py
@@ -0,0 +1,17 @@
+my_list=['Orange','Lemon','Lime','Tangerine','Grapefruit']
+print('my_list:',my_list)
+print('First element:',my_list[0])
+print('Last element:',my_list[-1])
+print('Sublist:',my_list[2:6])
+my_list[2]='Coconut'
+print('Modified list:',my_list)
+
+my_dict={'Orange':'апельсин',
+         'Lemon':'лимон',
+         'Lime':'лайм',
+         'Tangerine':'мандарин',
+         'Grapefruit':'грейпфрут'}
+print('Dictionary:',my_dict)
+print('Translation:',my_dict['Lemon'])
+my_dict['Coconut']='кокос'
+print('Modified dictionary:',my_dict)
