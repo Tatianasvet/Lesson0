feb16575b49c5caaab3ce52ab6542c0afd5666e0
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-05 19:18:59 +0500

    цикл

--- for_loop.py
+++ for_loop.py
@@ -1,2 +1,6 @@
-car_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-
+cars_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in cars_:
+    print('Я езжу на автомобиле марки', i)
+    cars_count += 10
+print(cars_count)
