6e073f57026c42bb0350ebba53f533c62a341fd7
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-06 20:37:24 +0500

    параметр по умолчанию

--- fun_1.py
+++ fun_1.py
@@ -1,11 +1,4 @@
-import random
-def loterre(mon, thue):
-    tickets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    win1 = random.choice(tickets)
-    tickets.remove(win1)
-    win2 = random.choice(tickets)
-    print(mon, thue)
-    return win1, win2
+def test(a =2, b = True):
+    print(a, b)
 
-win1, win2 = loterre('mon', 'thue')
-print(win1, win2)
+test()
