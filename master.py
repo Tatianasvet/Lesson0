1de439fef4503c1b0837b8629d27b6c51841026e
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-07 10:18:39 +0500

    цикл для смены хода

--- master.py
+++ master.py
@@ -7,7 +7,15 @@ area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"],]
 print("Добро пожаловать в крестики-нолики")
 print("----------------------------------")
 draw_area()
-row = int(input("Введите номер строки (1,2,3)")) - 1
-column = int(input("Введите номер строки (1,2,3)")) - 1
-area[row][column] = "X"
-draw_area()
+for turn in  range(1, 10):
+    print(f'Ход: {turn}')
+    if turn % 2 == 0:
+        turn_char = "0"
+        print(" Ходят нолики")
+    else:
+        turn_char = "X"
+        print(" Ходят крестики")
+    row = int(input("Введите номер строки (1,2,3)")) - 1
+    column = int(input("Введите номер строки (1,2,3)")) - 1
+    area[row][column] = turn_char
+    draw_area()
