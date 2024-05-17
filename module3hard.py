255d7e96a94e2738048b87f521a4400ba37d40a9
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-17 21:13:46 +0500

    int, float

--- module3hard.py
+++ module3hard.py
@@ -2,3 +2,22 @@
 def calculate_structure_sum(data_structure):
     total = 0
 
+    for component in  data_structure:
+        if isinstance(component, (int, float)):
+            total += component
+
+
+
+    return total
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
