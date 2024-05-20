cee15f7ec36f50eb42c80f93410f65f09ebe0f45
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-20 22:30:07 +0500

    2

--- module3_2hard.py
+++ module3_2hard.py
@@ -1,44 +1,12 @@
-def sum_data_structure(*args):
-    total_sum = 0
-    total_len = 0
+def flatten_list(data):
+    result = []
+    for item in data:
+        if isinstance(item, tuple) or isinstance(item, list):
+            result.extend(flatten_list(item))
+        else:
+            result.append(item)
+    return result
 
-    for arg in args:
-        if isinstance(arg, int):
-            total_sum += arg
-        elif isinstance(arg, str):
-            total_len += len(arg)
-        elif isinstance(arg, list) or isinstance(arg, tuple):
-            sub_sum, sub_len = sum_data_structure(*arg)
-            total_sum += sub_sum
-            total_len += sub_len
-        elif isinstance(arg, dict):
-            sub_sum, sub_len = sum_data_structure(*arg.values())
-            total_sum += sub_sum
-            total_len += sub_len
-
-    return total_sum, total_len
-
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
-
-total_sum, total_len = sum_data_structure(*data_structure)
-result = total_sum + total_len
-print("Total sum of all numbers:", total_sum)
-print("Total length of all strings:", total_len)
-print(result)
-
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
-result = sum_data_structure(data_structure)
-
-print(result)
+data = ((), [{(2, 'urban', ('urban2', 35))}])
+flat_data = flatten_list(data)
+print(flat_data)
