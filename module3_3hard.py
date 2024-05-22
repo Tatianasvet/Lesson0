6ce49ab4bdb6d33ac1699bb2fc229f3e2da7da71
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-05-22 08:42:53 +0500

    проба

--- module3_3hard.py
+++ module3_3hard.py
@@ -1,26 +1,21 @@
-def sum_str(component):
-    i = 0
-    i += len(component)
-    return i
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
 
-def sum_dict(component):
-    sum_ = 0
-    for keys in component.keys():
-        sum_ += len(keys)
-    for value in component.values():
-        sum_ += value
-    return sum_
-
-
-def calculate_structure_sum(*args):
+def calculate_structure_sum(data_structure):
     total = 0
 
-    for component in args:
-        if isinstance(component, (int, float)):
-            total += component
+    for data in data_structure:
+        if isinstance(data,list) or isinstance(data, set) or isinstance(data, tuple):
+
+
+        isinstance(component, (int, float)):
 
         elif isinstance(component, str):
-            sum_str(component)
 
         elif isinstance(component, dict):
             sum_dict(component)
