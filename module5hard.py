6ffde8326de817434c8cd5998ffd4d10ab1107d3
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-06-06 16:08:05 +0500

    class

---
+++ module5hard.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+class Video:
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos =[]
+        self.current_user = None
+
+    def log_in(self,):
