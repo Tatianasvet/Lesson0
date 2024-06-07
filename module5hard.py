7b1485dea54d23902c2eafc576b7fa2884e58ede
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-06-07 21:39:49 +0500

    Verification code

--- module5hard.py
+++ module5hard.py
@@ -31,7 +31,7 @@ class UrTube:
     def register(self, nickname, password, age):
         new_user = self.find_user(nickname)
         if new_user is not None:
-            print(f'Пользователь {new_user} уже существует')
+            print(f'Пользователь {new_user.nickname} уже существует')
             self.current_user = new_user
         else:
             new_user = User(nickname, password, age)
@@ -47,7 +47,7 @@ class UrTube:
     def log_out(self):
         self.current_user = None
 
-    def add(self, videos):
+    def add(self, *videos):
         for video in videos:
             if type(video) is Video:
                 self.videos.append(video)
@@ -79,3 +79,30 @@ class UrTube:
                         print(video.time_now, end=' ')
                     video.time_now = 0
                     print('Конец видео')
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
