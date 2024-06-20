8511e681e3754591f589fd64a198afb9afe5934d
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-06-20 23:01:32 +0500

    style

---
+++ module7_3.py
@@ -0,0 +1,13 @@
+
+team1_num = 6
+team2_num = 6
+print("В команде Мастера кода участников: %d !" % (team1_num))
+print('Итого сегодня в командах участников: %d и %d !' % (team1_num, team2_num))
+
+score1 = 40
+score2 = 42
+print('Команда Волшебники данных решила задач: {0} !'.format(score2))
+
+team1_time = 1552.512
+team2_time = 2153.31451
+print('Волшебники данных решили задачи за {0:4.1f} с !'.format(team2_time))
